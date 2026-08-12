ca5ca7e686cfec6eecc9e7223b7cbc5de95ee3d3
Harmanpreet Kaur <149608488+Harman-2@users.noreply.github.com>
2026-08-12 12:34:38 -0500

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,26 @@
+'''class Solution:
+    def isPalindrome(self, s: str):
+        newStr = ""
+        for c in s:
+            if c.isalnum(): # using built in alpnum function
+                newStr += c.lower()
+        return newStr == newStr[::-1]'''
+        
+class Solution:
+    def isPalindrome(self, s: str):
+        l, r = 0, len(s)-1
+        while l<r:
+            while l<r and not self.alphaNum(s[l]):
+                l += 1
+            while r>l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].upper() != s[r].upper():
+                return False
+            l, r = l+1, r-1
+        return True
+
+    
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
